fb4d199c8856e0f6e615881d491b7d39c862e1ae
Leandro Waserman <leandrowaserman@gmail.com>
2022-07-26 23:55:27 -0300

    entrega final

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config() 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 let MODE
 if(process.env.DEV_MODE=="produccion"){
     MODE = process.argv.slice(2) || "fork"
